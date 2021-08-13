4935494f96a350b33d70050c14bf874f478c1802
gauri <gauribathe7@gmail.com>
2021-08-13 10:52:33 +0530

    hiii

---
+++ Application/src/com/csi/app/Basic.java
@@ -0,0 +1,56 @@
+/*package com.csi.app;
+
+public class Basic {
+	public static void main(String[] args) {
+		
+	}
+}
+class Emp{
+	 // final  int empid;
+	  static int epid;
+	  
+
+	
+	  <!-- https://mvnrepository.com/artifact/junit/junit -->
+		  <dependency>
+		      <groupId>junit</groupId>
+		      <artifactId>junit</artifactId>
+		      <version>4.12</version>
+		      <scope>test</scope>
+		  </dependency>
+	  <groupId>junit</groupId>
+		<artifactId>junit</artifactId>
+		<version>4.12</version>
+		<scope>test</scope>
+	</dependency>
+	<dependency>
+<groupId>org.powermock</groupId>
+<artifactId>powermock-core</artifactId>
+<version>2.0.0-RC.4</version>
+<scope>test</scope>
+</dependency>
+<dependency>
+<groupId>org.powermock</groupId>
+<artifactId>powermock-module-junit4</artifactId>
+<version>2.0.0-beta.5</version>
+<scope>test</scope>
+</dependency>
+<dependency>
+<groupId>org.powermock</groupId>
+<artifactId>powermock-api-mockito2</artifactId>
+<version>2.0.0-beta.5</version>
+<scope>test</scope>
+</dependency>
+<dependency>
+<groupId>cglib</groupId>
+<artifactId>cglib</artifactId>
+<version>3.2.9</version>
+</dependency>
+<dependency>
+<groupId>org.mockito</groupId>
+<artifactId>mockito-inline</artifactId>
+<version>2.15.0</version>
+</dependency>
+
+	
+}*/
